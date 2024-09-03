0120c636b978b90dbb014d8dab92eeaea8345bbe
Detrementum <isantamariaa@correo.uss.cl>
2024-09-03 17:17:59 -0400

    codigo santamaria

---
+++ Ciudad.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Lab409
+ */
+public class Ciudad 
+{
+    private String nombre="";
+    private int numReg=0;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the numReg
+     */
+    public int getNumReg() {
+        return numReg;
+    }
+
+    /**
+     * @param numReg the numReg to set
+     */
+    public void setNumReg(int numReg) {
+        this.numReg = numReg;
+    }
+    
+}
