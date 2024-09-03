0eaeffde8a1b2f67d3172133500ccecc9e4ac450
Detrementum <isantamariaa@correo.uss.cl>
2024-09-03 17:28:54 -0400

    cambios

---
+++ Tareaa/src/Clases/Persona.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Lab409
+ */
+public class Persona 
+{
+    private String rut="";
+    private String nombre="";
+    private int edad=0;
+
+    /**
+     * @return the rut
+     */
+    public String getRut() {
+        return rut;
+    }
+
+    /**
+     * @param rut the rut to set
+     */
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+    
+    
+}
